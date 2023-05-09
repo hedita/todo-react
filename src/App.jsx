912767ccb904ac7275c4a55cd3e37b1a4dd6dd04
Hedita <hedi.takrami@gmail.com>
2023-05-10 00:06:59 +0330

    refactore the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddPage from "./pages/AddPage/AddPage";
-import AllPage from "./pages/AllPage/AllPage";
-import CompletedPage from "./pages/CompletedPage/CompletedPage";
-import UnCompletedPage from "./pages/UnCompletedPage/UnCompletedPage";
+import AllCompletedUncompleted from "./pages/AllCompletedUncompleted/AllCompletedUncompleted";
 import ConfigurationPage from "./pages/ConfigurationPage/ConfigurationPage";
-import Main from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -20,9 +17,7 @@ const App = () => {
               <Navbar />
               <Routes>
                 <Route path="/add" element={<AddPage />} />
-                <Route path="/" element={<AllPage />} />
-                <Route path="/completed" element={<CompletedPage />} />
-                <Route path="/uncompleted" element={<UnCompletedPage />} />
+                <Route path="/" element={<AllCompletedUncompleted />} />
                 <Route path="/configuration" element={<ConfigurationPage />} />
               </Routes>
             </main>
--- src/pages/UnCompletedPage/UnCompletedPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UnCompletedPage = () => {
-  return <h2>uncompleted</h2>;
-};
-
-export default UnCompletedPage;
