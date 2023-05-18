42dc7e4699adb167784d2862c91e2e37cab5196a
Hedita <hedi.takrami@gmail.com>
2023-05-18 11:23:04 +0330

    refactore the code

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,28 +1,8 @@
 import React from "react";
-import { useEffect, useState } from "react";
-import { apiBaseUrl } from "../../../config";
 import "./TodoItem.scss";
 
-const TodoItem = () => {
-  const [tasks, setTasks] = useState([]);
-	const [error, setError] = useState(null);
-
-  useEffect(() => {
-    getTasks();
-  });
-
-  async function getTasks() {
-		try {
-			const response = await fetch(`${apiBaseUrl}/todos`);
-			const { data } = await response.json();
-			setTasks(data);
-		} catch (error) {
-			setError(error.message)
-		} 
-  }
-  return tasks.map(({ text, id }) => {
-    return <li key={id} className="todo-item">{text}</li>;
-  });
+const TodoItem = ({ text }) => {
+  return <li className="todo-item">{text}</li>;
 };
 
 export default TodoItem;
--- src/pages/TodoList/TodoList.jsx
+++ src/pages/TodoList/TodoList.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import TodoItem from "../../components/TodoItem/TodoItem";
+import { apiBaseUrl } from "../../../config";
 
 const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  async function getTasks() {
+    try {
+      const response = await fetch(`${apiBaseUrl}/todos`);
+      const { data } = await response.json();
+      setTasks(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
   return (
-    <ul>
-      <TodoItem />
-    </ul>
+    <>
+      {error && <p className="error-message">Something went wrong!</p>}
+      <ul>
+        {tasks.map(({ text, id }) => {
+          return <TodoItem key={id} text={text} />;
+        })}
+      </ul>
+    </>
   );
 };
 
