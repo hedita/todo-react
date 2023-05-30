2f7040cf035e9225cfe4bfc1d49d5c3a5c8f38fa
Hedita <hedi.takrami@gmail.com>
2023-05-30 10:47:59 +0330

    emplement checkbox event

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./TodoItem.scss";
-import { useState, useContext } from "react";
+import { useState } from "react";
 import { apiBaseUrl, requestDefaultHeaders } from "../../../config";
 import { formatDate } from "../../utils";
 
 const TodoItem = ({ text, createdAt, taskId, getTasks }) => {
   const [error, setError] = useState("");
+  const [isDone, setIsDone] = useState(false);
 
   const deleteTodo = () => {
     try {
@@ -20,10 +21,25 @@ const TodoItem = ({ text, createdAt, taskId, getTasks }) => {
     }
   };
 
+  const updateStatus = () => {
+    try {
+      fetch(`${apiBaseUrl}/todos/${taskId}`, {
+        method: "PATCH",
+        body: JSON.stringify({ isDone }),
+        headers: requestDefaultHeaders,
+      });
+      setIsDone(!isDone);
+      getTasks();
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <>
       {error && <p className="error-message">Something went wrong!</p>}
       <li title={formatDate(createdAt)} className="todo-item">
+        <input className="isDone-checkbox" type="checkbox" onClick={updateStatus} />
         {text}
         <button className="delete-button" onClick={deleteTodo}>
           Delete
