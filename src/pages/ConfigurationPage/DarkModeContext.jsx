9cfa48c72e2be85bd1de8376715f54ddc4183f4d
Hedita <hedi.takrami@gmail.com>
2023-06-10 23:12:31 +0330

    add localstorage

--- src/pages/ConfigurationPage/DarkModeContext.jsx
+++ src/pages/ConfigurationPage/DarkModeContext.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createContext, useState } from "react";
 
 const DarkModeContext = createContext(false);
 
 const DarkModeProvider = (props) => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(
+    () => JSON.parse(localStorage.getItem("darkMode")) || false
+  );
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
+
   const toggleTheme = () => {
     setDarkMode(!darkMode);
   };
